307b9f2e0fa228825049153dd3a6b32f341405f2
felipealonso03 <alonsolareo@udesa.edu.ar>
2022-11-21 11:25:20 -0300

    push

--- search-results.js
+++ search-results.js
@@ -4,7 +4,7 @@ let apiKey = "?api_key=ba0b591fbb4dcbf21e7a279fceca5d5e"
 let movie = location.search
 console.log(location)
 let objMovie = new URLSearchParams(movie)
-let keyword = objMovie.get ("pelicula")
+let keyword = objMovie.get ("q")
 
 fetch(`${endpoint}${apiKey}&query=${keyword}`)
 .then(function(resp){
