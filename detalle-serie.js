7a02125818840403aadf624f9a0a51c5ad38e00e
Lucioghidini <lghidini@udesa.edu.ar>
2022-11-22 23:22:27 -0300

    cambios

--- detalle-serie.js
+++ detalle-serie.js
@@ -133,8 +133,6 @@ fetch (trailer)
 
 let boton = document.querySelector('.botonRecom')
 
-
-
 boton.addEventListener('click', function(){
   let img = `https://image.tmdb.org/t/p/original`
   let recomendacionesSeriesSection = document.getElementById("recomendacionesSerie")
